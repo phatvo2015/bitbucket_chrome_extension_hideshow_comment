570b17cbf708fa2f5de1ef9441ae123846b7eaf7
Peter Vo <vtphat19@gmail.com>
2023-11-19 21:40:38 +0700

    allow to copy to clipboard

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ function hideElement(element) {
     element.style.display = 'none';
 }
 
+// function copyClipboard() {
+//     const copyContent = async () => {
+//         try {
+//           await navigator.clipboard.writeText(text);
+//           console.log('Content copied to clipboard');
+//         } catch (err) {
+//           console.error('Failed to copy: ', err);
+//         }
+//       }
+// }
+
 function commentDisplayToggle() {
     var comment_card = this.parentElement.parentElement.parentElement;
     var comment_contents = comment_card.querySelectorAll(".ak-renderer-wrapper");
@@ -30,6 +41,34 @@ var conversation_blocks = document.querySelectorAll('section[data-qa="conversati
 for (var i = 0; i < conversation_blocks.length; i++) {
 
     var timestamps = conversation_blocks[i].querySelectorAll('a[href*="comment"]');
+    var numberOfCommentElement = timestamps.length;
+
+    var commentButtons = conversation_blocks[i].querySelectorAll('button');
+    for (var j = 0; j < commentButtons.length; j++) {
+        if (commentButtons[j].textContent == "Create task") {
+            var buttonContainer = commentButtons[j].parentElement.parentElement.parentElement;
+
+            if (buttonContainer.querySelector('button[extension-custom-created="true"]') == null) {
+                var copyButtonDiv = commentButtons[j].parentElement.parentElement.cloneNode(true);
+                var copyButton = copyButtonDiv.querySelector("button");
+                copyButton.textContent = "Copy to clipboard";
+                copyButton.setAttribute("extension-custom-created", "true")
+
+                copyButton.onclick = function () {
+                    var commentContainer = this.parentElement.parentElement.parentElement.parentElement;
+                    var text = commentContainer.querySelector('div[class*="is-comment"]').textContent;
+                    navigator.clipboard.writeText(text).then(function () {
+                        console.log("Link copied to clipboard: " + text);
+                    });
+                };
+                buttonContainer.insertBefore(copyButtonDiv, buttonContainer.firstChild);
+            }
+        }
+
+    }
+
+
+
     for (var j = 0; j < timestamps.length; j++) {
         timestamps[j].addEventListener(
             "click",
